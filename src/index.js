99a4577a40519e25c57d70ed15dfefff268066fc
Michael Lihs <michael.lihs@thoughtworks.com>
2022-05-05 09:45:03 +0200

    fix import problem for ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {DateTime, Duration} from 'luxon';
 import {Button, Form, Row} from "react-bootstrap";
 import {utils, writeFileXLSX} from 'xlsx';
